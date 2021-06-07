d392c58a06d247aabd079aceec51d111cd71e9aa
fanyixin0428 <fanyixin0428@gmail.com>
2021-06-07 20:51:06 +0800

    rpg

---
+++ RPG Tutorial/Assets/Scripts/Item/Item.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName ="New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+
+    public virtual void Use()
+    {
+        Debug.Log("Using" + name);
+        // Use the item
+        // SOmething might happen
+    }
+
+    public void RemoveFromInventory()
+    {
+        Inventory.instance.Removed(this);
+
+    }
+
+
+}
---
+++ RPG Tutorial/Assets/Scripts/Item/ItemPickup.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+
+    }
+
+    void PickUp()
+    {
+       
+        Debug.Log("Picking up "+ item.name);
+        bool wasPickedUP = Inventory.instance.Add(item);
+        //Add to inventory
+        if(wasPickedUP)
+            Destroy(gameObject);
+    }
+}
